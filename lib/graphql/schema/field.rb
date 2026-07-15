b70e40c618a7af7cc8e116994615d7870eb91cd2
Katja Cresanti <108829316+katjacresanti@users.noreply.github.com>
2026-07-15 13:38:03 -0700

    Revert "Update field.rb"
    This reverts commit 64126342fcf0e7eaaa843ca2b33425484eb2b750.

--- lib/graphql/schema/field.rb
+++ lib/graphql/schema/field.rb
@@ -44,11 +44,7 @@ module GraphQL
       # @param mutation [Class] A {GraphQL::Schema::Mutation} class to use for field configuration
       # @return [GraphQL::Schema:Field] an instance of `self
       # @see {.initialize} for other options
-      def self.from_options(name = nil, type = nil, desc = nil, options = nil, resolver: nil, mutation: nil, **kwargs, &block)
-        if options
-          kwargs = options.merge(kwargs)
-        end
-
+      def self.from_options(name = nil, type = nil, desc = nil, resolver: nil, mutation: nil, **kwargs, &block)
         if (parent_config = resolver || mutation)
           # Get the parent config, merge in local overrides
           kwargs = parent_config.field_options.merge(kwargs)
